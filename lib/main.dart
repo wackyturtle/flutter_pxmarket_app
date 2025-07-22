182f944c81f1c6ec56338129327a607f28e08a48
wackyturtle <wcytutl01@gmail.com>
2025-07-22 13:58:26 +0900

    delete: 불필요코드 삭제

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_pxmarket_app/product_list_page/product_list_page.dart';
 
 void main() {
-  runApp(const PxMarketApp());
+  runApp(const MyApp());
 }
 
-class PxMarketApp extends StatelessWidget {
-  const PxMarketApp({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        inputDecorationTheme: InputDecorationTheme(
+          labelStyle: TextStyle(color: Color.fromARGB(255, 58, 115, 37)),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Color.fromARGB(255, 58, 115, 37)),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey),
+          ),
+        ),
+        dialogTheme: DialogThemeData(
+          backgroundColor: Colors.yellow[50],
+          titleTextStyle: TextStyle(
+            //팝업창제목스타일
+            color: Color(0xFF3E5934),
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       home: ProductListPage(),
       debugShowCheckedModeBanner: false,
     );
