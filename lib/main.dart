bfa2ff9b00b6634b6455953020b63a50e4917261
su <su@naver.com>
2025-07-21 17:35:10 -0700

    feat : ui

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_pxmarket_app/product_list_page/product_list_page.dart';
 
 void main() {
-  runApp(const PxMarketApp());
+  runApp(MyApp());
 }
 
-class PxMarketApp extends StatelessWidget {
-  const PxMarketApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
